8e69d66796328f6721502693cbbe650a6f9b6677
AnhNV <vananhn1703@gmail.com>
2019-02-18 16:58:06 +0700

    update reload balance

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { UserWallet } from '../models/user-wallet';
-import { UpdateBalanceService } from '../services/update-balance.service';
-import { BuyService } from '../services/buy.service';
-import { TransferService } from '../services/transfer.service';
-import { UserInfoService } from '../services/user-info.service';
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Location } from '@angular/common';
+import { UserWallet } from "../models/user-wallet";
+import { UpdateBalanceService } from "../services/update-balance.service";
+import { BuyService } from "../services/buy.service";
+import { TransferService } from "../services/transfer.service";
+import { UserInfoService } from "../services/user-info.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+
+  
   userWallet: UserWallet;
   walletBalance: UserWallet;
   listTransaction = [];
   balance: Number;
   walletId: String;
   balance_available: Number;
-  userchoices= ["Tea", "Coffee", "Yogurt"];
-  userchoice = 'Tea';
+  userchoices = ["Tea", "Coffee", "Yogurt"];
+  userchoice = "Tea";
   simpleItems = [];
   successfulList: String;
   isShow = true;
-  constructor(private updateBalanceService: UpdateBalanceService, private buyService: BuyService, private transferService: TransferService, private userInfoService: UserInfoService) {
+  constructor(
+    private updateBalanceService: UpdateBalanceService,
+    private buyService: BuyService,
+    private transferService: TransferService,
+    private userInfoService: UserInfoService,
+  ) {
     this.userWallet = JSON.parse(localStorage.getItem("userWallet"));
     this.walletId = this.userWallet.walletId;
-    
-   }
+  }
 
   ngOnInit() {
     this.simpleItems = ["Tea", "Coffee", "Yogurt"];
@@ -35,34 +41,58 @@ export class HomeComponent implements OnInit {
     this.getListTransaction();
   }
 
-  updateBalance(walletId: String){
-    this.updateBalanceService.updateBalance(walletId)
-      .subscribe(balance => {this.walletBalance = balance});
+  updateBalance(walletId: String) {
+    this.updateBalanceService.updateBalance(walletId).subscribe(balance => {
+      this.walletBalance = balance;
+    });
   }
 
-  buy(quantity: Number, userchoice: String, real_balance: Number){
-    debugger
-    this.buyService.buy(quantity, userchoice, this.userWallet.publicKey, real_balance)
-      .subscribe(balance => { this.walletBalance = balance});
-      alert(this.walletBalance.message);
+  buy(quantity: Number, userchoice: String, real_balance: Number) {
+    debugger;
+    this.buyService
+      .buy(quantity, userchoice, this.userWallet.publicKey, real_balance)
+      .subscribe(balance => {
+        this.walletBalance = balance;
+        alert(this.walletBalance.message);
+        // const reload = setInterval(() => {
+          //   this.updateBalance(this.walletId);
+          //   clearInterval(reload);
+          // }, 10000);
+        });
+    // this.pageRefresh();
+    this.updateBalance(this.walletId);
   }
 
-  transfer(amount: Number, receiver: String, real_balance: Number){
-    debugger
-    this.transferService.transfer(amount, receiver, this.userWallet.publicKey, real_balance)
-      .subscribe(balance => { this.walletBalance = balance});
-      alert(this.walletBalance.message);
+  transfer(amount: Number, receiver: String, real_balance: Number) {
+    debugger;
+    this.transferService
+      .transfer(amount, receiver, this.userWallet.publicKey, real_balance)
+      .subscribe(balance => {
+        this.walletBalance = balance;
+        alert(this.walletBalance.message);
+      });
+    // this.pageRefresh();
+    this.updateBalance(this.walletId);
   }
 
-  getSuccessfulList(){
-    debugger
-    this.userInfoService.getSuccessfulList(this.userWallet.walletId)
-      .subscribe(succesfulList => {this.successfulList = succesfulList});
+
+  getSuccessfulList() {
+    debugger;
+    this.userInfoService
+      .getSuccessfulList(this.userWallet.walletId)
+      .subscribe(succesfulList => {
+        this.successfulList = succesfulList;
+      });
     this.isShow = !this.isShow;
+    // this.pageRefresh();
+    this.updateBalance(this.walletId);
   }
 
-  getListTransaction(){
-    this.userInfoService.getListTransaction()
-      .subscribe(result => {this.listTransaction = result});
+  getListTransaction() {
+    this.userInfoService.getListTransaction().subscribe(result => {
+      this.listTransaction = result;
+    });
+    // this.pageRefresh();
+    this.updateBalance(this.walletId);
   }
 }
